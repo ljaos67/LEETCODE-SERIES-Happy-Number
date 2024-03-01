b1d4a6104cf4b7b423ada256e95ecefcab07a6d0
Leonart Jaos <70718641+ljaos67@users.noreply.github.com>
2024-03-01 12:57:27 -0600

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsHappy(int n) {
+        var numbers = new HashSet<int>();
+        while(n > 1 && numbers.Add(n))
+        {
+            var sum = 0;
+            while(n > 0)
+            {
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
